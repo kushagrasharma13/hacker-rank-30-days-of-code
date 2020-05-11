f42400115fb7ff3d3db046bce5fc7762da3c1bea
kushagra sharma <61291218+kushagrasharma13@users.noreply.github.com>
2020-05-12 00:11:38 +0530

    Add files via upload

---
+++ Day 4 Class vs. Instance.py
@@ -0,0 +1,16 @@
+class Person:
+    def __init__(self,initialAge):
+        if initialAge<0:
+            print('Age is not valid, setting age to 0.')
+            self.nage=0
+        else:
+            self.nage=initialAge
+    def amIOld(self):
+        if self.nage<13:
+            print('You are young.')
+        elif self.nage>=13 and self.nage<18:
+            print('You are a teenager.')
+        else:
+            print('You are old.')
+    def yearPasses(self):
+        self.nage+=1
