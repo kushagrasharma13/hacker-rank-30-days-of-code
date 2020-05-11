5a88b7101357852064693dceff5c89711b904815
kushagra sharma <61291218+kushagrasharma13@users.noreply.github.com>
2020-05-12 00:12:21 +0530

    Update Day 4 Class vs. Instance.py

--- Day 4 Class vs. Instance.py
+++ Day 4 Class vs. Instance.py
@@ -14,3 +14,13 @@ class Person:
             print('You are old.')
     def yearPasses(self):
         self.nage+=1
+
+t = int(input())
+for i in range(0, t):
+    age = int(input())         
+    p = Person(age)  
+    p.amIOld()
+    for j in range(0, 3):
+        p.yearPasses()       
+    p.amIOld()
+    print("")
